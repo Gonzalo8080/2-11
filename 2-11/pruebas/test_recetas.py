d17522baf1536a660ab836be83c6225331e2f639
Gonzalo8080 <gonzalo.alsogaray@davinci.edu.ar>
2024-12-12 10:22:55 -0300

    asas

--- 2-11/pruebas/test_recetas.py
+++ 2-11/pruebas/test_recetas.py
@@ -4,5 +4,5 @@ from ingresar import ingreso
 class test_recetas(unittest.TestCase):
     def receta_valida(self):
         self.assertTrue(ingreso("nombre", "ingrediente1, ingrediente2", "categoria"))
-        self.assertTrue(ingreso("algo dulce", "azucar, miel", "dulce"))
+        self.assertTrue(ingreso("algo dulce", "azucar, leche", "dulce"))
         self.assertTrue(ingreso("algo salado", "sal, aceite", "salado"))
